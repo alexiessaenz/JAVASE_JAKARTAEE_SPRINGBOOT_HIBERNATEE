6741063c4db03f3b729d96e1b542d1847b5772e8
alexiessaenz <00400815@uca.edu.sv>
2023-06-02 15:33:44 -0600

    fix: CRUD

---
+++ CRUds/src/main/java/org/example/repositorio/OrdenableRepositorio.java
@@ -0,0 +1,9 @@
+package org.example.repositorio;
+
+import org.example.modelo.Cliente;
+
+import java.util.List;
+
+public interface OrdenableRepositorio {
+    List<Cliente> listar(String campo, Direccion dir);
+}
